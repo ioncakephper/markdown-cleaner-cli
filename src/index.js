89c4ba27dc8ef4228b32288a142c5344684cad18
Ion Gireada <ioncakephper@users.noreply.github.com>
2025-08-13 14:21:28 +0300

    feat: add error handling to run function

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@
 const createProgram = require('./program');
 
 function run() {
-  const program = createProgram();
-  program.parse(process.argv);
+  try {
+    const program = createProgram();
+    program.parse(process.argv);
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 run();
