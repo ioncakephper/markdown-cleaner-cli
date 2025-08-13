7258643f219caf085876d90b359219fe4b0da821
Ion Gireada <ioncakephper@users.noreply.github.com>
2025-08-13 14:49:30 +0300

    feat: add global options to program

--- src/program.js
+++ src/program.js
@@ -7,7 +7,10 @@ function createProgram() {
     .name(Object.keys(myPackage.bin)[0])
     .version(myPackage.version)
     .description('A CLI tool to clean and format Markdown files.')
-    .configureHelp({ sortOptions: true, sortCommands: true });
+    .configureHelp({ sortOptions: true, sortCommands: true })
+    .option('-v, --verbose', 'enable verbose output')
+    .option('--debug', 'enable debug output')
+    .option('--silent', 'suppress all output');
 
   program
     .command('clean <file>')
